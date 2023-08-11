7c88ce45a77a6833980a9fee05d11ff0fd8d682c
leeminhyuk93 <dlalsgur1003@naver.com>
2023-08-11 22:42:57 +0900

    Initial commit

---
+++ src/main/java/com/lee/book/springboot/service/posts/PostsService.java
@@ -0,0 +1,53 @@
+package com.lee.book.springboot.service.posts;
+
+import com.lee.book.springboot.domain.posts.Posts;
+import com.lee.book.springboot.domain.posts.PostsRepository;
+import com.lee.book.springboot.web.dto.PostsListResponseDto;
+import com.lee.book.springboot.web.dto.PostsResponseDto;
+import com.lee.book.springboot.web.dto.PostsSaveRequestDto;
+import com.lee.book.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+        return id;
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new) // stream()의 map()으로 순회하는 객체는 Entity이다. 생성자의 파라미터로 Entity가 입력된다.
+                .collect(Collectors.toList());
+    }
+
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다."));
+        postsRepository.delete(posts);
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+        return new PostsResponseDto(entity);
+    }
+}
